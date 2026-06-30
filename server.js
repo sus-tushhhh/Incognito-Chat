de51844e945075f40a4b43dd7c14259b355c072c
sus_tushhhh <tushant0430@gmail.com>
2026-06-30 16:31:47 +0530

    socket

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import http from "http";
+import { Server } from "socket.io";
 import messageRoutes from "./routes/messageRoutes.js";
 import roomRoutes from "./routes/roomRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = process.env.PORT || 3000;
 
+app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/messages", messageRoutes);
 app.use("/api/room", roomRoutes);
 app.use(errorHandler);
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+    res.sendFile("/public/index.html");
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
